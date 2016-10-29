bd45be9e7534da4c4690f3be3b416d810b92ff59
hector-isaac <hectorisaac@hotmail.com>
2016-10-29 11:31:07 -0500

    Hola mundo

--- ejemploproyecto/ejemploproyecto/Form1.cs
+++ ejemploproyecto/ejemploproyecto/Form1.cs
@@ -16,5 +16,10 @@ namespace ejemploproyecto
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello FIME");
+        }
     }
 }
